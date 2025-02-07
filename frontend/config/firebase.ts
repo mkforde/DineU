f27b4592593709af8e645fd67e55f9994b94fede
Michael Forde <michael.kenneth.forde@gmail.com>
2025-02-07 02:43:52 -0500

    Save work firebase

---
+++ frontend/config/firebase.ts
@@ -0,0 +1,29 @@
+// Import expo environmental variables
+import Constants from 'expo-constants';
+
+// if getting error with typescript https://github.com/firebase/firebase-js-sdk/issues/8332
+
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistance } from "firebase/auth";
+import { ReactNativeAsyncStorage } from '@react-native-async-storage/async-storage';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgz_SroYsghGH3cmK0TE5AtBthi1IHVSM",
+  authDomain: "devfest-2025-792b6.firebaseapp.com",
+  projectId: "devfest-2025-792b6",
+  storageBucket: "devfest-2025-792b6.firebasestorage.app",
+  messagingSenderId: "414709733802",
+  appId: "1:414709733802:web:80ed8d3274162fbca2b3d3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistance(ReactNativeAsyncStorage)
+});
